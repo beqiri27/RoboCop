8ea93153245b3c305ca74d035ab0418b9f5ea395
Alessio Beqiri <alebeqi27@gmail.com>
2026-05-22 08:51:39 +0200

    update db-init.sql

--- src/Model/db-init.sql
+++ src/Model/db-init.sql
@@ -1,5 +1,17 @@
-CREATE TABLE user(
+CREATE TABLE utente(
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL UNIQUE,
-    password VARCHAR(30)
-)
+    password_utente VARCHAR(30) NOT NULL,
+    ruolo ENUM('user','admin') NOT NULL
+);
+
+CREATE TABLE segnalazione(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(30) NOT NULL,
+    descrizione TEXT NOT NULL,
+    data_creazione DATE NOT NULL,
+    CONSTRAINT fk_utente FOREIGN KEY (id_utente) REFERENCES utente(id)
+    coordinate_x FLOAT NOT NULL,
+    coordinate_y FLOAT NOT NULL,
+    criticita ENUM('bassa', 'media', 'alta') NOT NULL
+);
