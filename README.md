7775378accb5423eb7ff4c0a6b4b53820681bc8e
Francesco Grassi <grassifrancesco07@protonmail.com>
2026-05-22 09:44:47 +0200

    modificato main.py

--- README.md
+++ README.md
@@ -6,6 +6,13 @@
 ## Frontend
 1. React Native
 
+## TODO
+Gelatti: 
+Beqiri: 
+Grassi: definizione rotte,
+
+---
+
 # Welcome to your Expo app 👋
 
 This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
--- src/backend/main.py
+++ src/backend/main.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World! afgsahf9awitgòo</p>"
+
+
+@app.post("/register")
+def register():
+    return {"success": True}
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000, debug=True)
